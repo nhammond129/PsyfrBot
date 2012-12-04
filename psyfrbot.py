ae0db7fe1960e675f361625a4793ec80da3876aa
Nullspeaker <nhammond129@gmail.com>
2012-12-03 21:17:38 -0600

    Improved exception throwing

--- psyfrbot.py
+++ psyfrbot.py
@@ -2,7 +2,25 @@ import _thread
 import shelve
 import sys
 import os
+import traceback
 from datetime import datetime
+
+def FancyExceptionThrower(func,*args, **kwargs):
+  def wrapper(*args, **kwargs):
+    try:
+      func(*args, **kwargs)
+    except Exception as exc:
+      traceback.print_exc()
+      print("Exception occurred in %s"%func.__name__)
+      if len(args)>0:
+        print("with args: {\n  "+"\n  ".join([repr(i) for i in args])+"\n}")
+      if len(kwargs)>0:
+        print("with kwargs: {\n  "+
+          "\n  ".join(["%s = %s"%(i,repr(j)) for i,j in kwargs.items()])
+          +"\n}")
+      input("Awaiting dismissal...")
+      sys.exit(1)
+  return wrapper
 
 # There's a better way to do this, yes?
 #  If so, someone please fork and request a pull/open an issue with the fix.
@@ -14,10 +32,12 @@ sys.path=osp
 isMatch=lambda x,y:x.lower()==y.lower()
 
 class user:
+  @FancyExceptionThrower
   def __init__(self,lvl):
     self.level=lvl
 
 class botprocessor:
+  @FancyExceptionThrower
   def __init__(self,
       commandSym="!", #Command symbol, like "!dothings"
       configlocation="config.cfg",
@@ -35,67 +55,79 @@ class botprocessor:
     self.pluginManager=pluginsys.pluginManager()
     self.users={}
     self.config={}
-  
+    
+  @FancyExceptionThrower
   def ignore(self,name):
     if name.lower() not in self._ignore:
       self._ignore.append(name.lower())
   
+  @FancyExceptionThrower
   def saveconfig(self):
     conffile=shelve.open(self._configloc)
     for i in self.config:
       self.conffile[i]=self.config[i]
     conffile.close()
   
+  @FancyExceptionThrower
   def loadconfig(self):
     conffile=shelve.open(self._configloc)
     for i in conffile:
       self.config[i]=conffile[i]
     conffile.close()
   
+  @FancyExceptionThrower
   def saveusers(self):
     usrfile=shelve.open(self._usersloc)
     for i in self.users:
       usrfile[i]=self.users[i]
     usrfile.close()
-    
+  
+  @FancyExceptionThrower
   def loadusers(self):
     usrfile=shelve.open(self._usersloc)
     for i in usrfile:
       self.users[i]=usrfile[i]
     usrfile.close()
     
+  @FancyExceptionThrower
   def MsgLog(self,msg):
     logF=open("logs/"+"LOG_"+datetime.now().strftime("%b-%d-%Y").upper()+".txt","a+")
     logF.write(datetime.now().strftime("{%H:%M:%S} ")+msg+"\n")
     logF.flush()
     logF.close()
   
+  @FancyExceptionThrower
   def Save(self):
     self.saveconfig()
     self.saveusers()
-    
+  
+  @FancyExceptionThrower
   def Load(self):
     self.loadconfig()
     self.loadusers()
   
+  @FancyExceptionThrower
   def getuser(self,usrname):
     usrname=usrname.lower()
     if usrname in self.users.keys():
       return self.users[usrname]
     else:
       return None
-    
+      
+  @FancyExceptionThrower
   def adduser(self,usrname,lvl=0):
     usrname=usrname.lower()
     if self.getuser(usrname):
       return None
     else:
       self.users[usrname]=user(lvl)
-    
+  
+  @FancyExceptionThrower
   def messageparser(self,message):
     #overriding encouraged
     return message.split(" ")
-    
+  
+  @FancyExceptionThrower
   def get_response(self,message,usrname):
     #probably want to override if messageparser is overridden.
     if self.doLogging:
@@ -117,7 +149,8 @@ class botprocessor:
         else:
           return "Invalid permission rank."
     return self.defaultError
-    
+  
+  @FancyExceptionThrower
   def reply(self,message,usrname):
     if self._respondFunc==None:
       return None
